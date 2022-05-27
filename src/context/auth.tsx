da89840d5118b366058260892aa83230278ee289
Matheus Ramos Kafuri <matheuskafuri@Matheuss-MacBook-Pro.local>
2022-05-26 21:37:31 -0300

    firebase users collection creation

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,41 +1,10 @@
-import React, { createContext, ReactNode, useEffect, useState } from "react";
+import React, { createContext, ReactNode, useState } from "react";
 import { User } from "../types/user";
-
-import { getApps, initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { FacebookAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import {
-  addDoc,
-  collection,
-  doc,
-  getFirestore,
-  setDoc,
-} from "firebase/firestore";
+import { auth, db } from "../utility/firebase.config";
+import { FacebookAuthProvider, signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import { destroyCookie, setCookie } from "nookies";
 import { useRouter } from "next/router";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
-
-if (!getApps.length) {
-  initializeApp(firebaseConfig);
-  if (typeof window !== "undefined") {
-    if ("measurementId" in firebaseConfig) {
-      getAnalytics();
-    }
-  }
-}
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore(app);
 
 type AuthContextData = {
   user: User | null;
