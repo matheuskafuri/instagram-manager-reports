c4b9c4a89020c2ce772f7eb498e8b524b244ca0a
Matheus Ramos Kafuri <kafurimatheus@gmail.com>
2022-06-10 15:56:28 -0300

    router.push bug fix

--- src/components/AppMenu/index.tsx
+++ src/components/AppMenu/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import theme from "../../styles/theme/lightThemeOptions";
 import {
@@ -14,6 +14,7 @@ import {
 } from "@mui/material";
 import { SiteNavBar } from "../SiteNavBar";
 import StarIcon from "@mui/icons-material/Star";
+import { Loader } from "../Loader";
 
 const tiers = [
   {
@@ -24,7 +25,7 @@ const tiers = [
       "Exporte suas tabelas de dados para o Excel.",
     ],
     buttonText: "Ir para o Dashboard",
-    buttonLink: "dashboard",
+    buttonLink: "/dashboard",
     buttonVariant: "contained",
     buttonColor: "royalblue",
     backgroundColor: "theme.palette.background.paper",
@@ -37,7 +38,7 @@ const tiers = [
       "Gere relatórios para todas as suas contas.",
     ],
     buttonText: "Adicionar Contas",
-    buttonLink: "add-accounts",
+    buttonLink: "/add-accounts",
     buttonVariant: "contained",
     buttonColor: "#e91e63",
     backgroundColor: "theme.palette.background.paper",
@@ -46,6 +47,12 @@ const tiers = [
 
 const AppMenu = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleNavigation = (endpoint: string) => {
+    setLoading(true);
+    router.push(endpoint);
+  };
   return (
     <>
       <SiteNavBar />
@@ -56,71 +63,75 @@ const AppMenu = () => {
           marginTop: "2.5rem",
         }}
       >
-        <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid
-              item
-              key={tier.title}
-              xs={12}
-              md={6} //with 3 cards, this is should be 4
-            >
-              <Card
-                sx={{
-                  backgroundColor: tier.backgroundColor,
-                  borderRadius: theme.shape.borderRadius,
-                  boxShadow: theme.shadows[5],
-                  padding: theme.spacing(2),
-                  color: theme.palette.text.primary,
-                }}
+        {loading ? (
+          <Loader />
+        ) : (
+          <Grid container spacing={5} alignItems="flex-end">
+            {tiers.map((tier) => (
+              // Enterprise card is full width at sm breakpoint
+              <Grid
+                item
+                key={tier.title}
+                xs={12}
+                md={6} //with 3 cards, this is should be 4
               >
-                <CardHeader
-                  title={tier.title}
-                  titleTypographyProps={{
-                    align: "center",
-                    fontWeight: "bold",
-                    color: theme.palette.primary.main,
+                <Card
+                  sx={{
+                    backgroundColor: tier.backgroundColor,
+                    borderRadius: theme.shape.borderRadius,
+                    boxShadow: theme.shadows[5],
+                    padding: theme.spacing(2),
+                    color: theme.palette.text.primary,
                   }}
-                />
-                <CardContent>
-                  {tier.description.map((line) => (
-                    <Box
-                      sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        mb: 2,
-                      }}
-                      key={line}
-                    >
-                      <StarIcon
-                        sx={{ mr: 1, color: theme.palette.primary.main }}
-                      />
-
-                      <Typography
-                        variant="subtitle1"
-                        align="center"
-                        fontWeight="semiBold"
+                >
+                  <CardHeader
+                    title={tier.title}
+                    titleTypographyProps={{
+                      align: "center",
+                      fontWeight: "bold",
+                      color: theme.palette.primary.main,
+                    }}
+                  />
+                  <CardContent>
+                    {tier.description.map((line) => (
+                      <Box
+                        sx={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          mb: 2,
+                        }}
+                        key={line}
                       >
-                        {line}
-                      </Typography>
-                    </Box>
-                  ))}
-                </CardContent>
-                <CardActions>
-                  <Button
-                    fullWidth
-                    variant={tier.buttonVariant as "outlined" | "contained"}
-                    onClick={() => router.push(`/${tier.buttonLink}`)}
-                    sx={{ backgroundColor: tier.buttonColor }}
-                  >
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+                        <StarIcon
+                          sx={{ mr: 1, color: theme.palette.primary.main }}
+                        />
+
+                        <Typography
+                          variant="subtitle1"
+                          align="center"
+                          fontWeight="semiBold"
+                        >
+                          {line}
+                        </Typography>
+                      </Box>
+                    ))}
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      fullWidth
+                      variant={tier.buttonVariant as "outlined" | "contained"}
+                      onClick={() => handleNavigation(`${tier.buttonLink}`)}
+                      sx={{ backgroundColor: tier.buttonColor }}
+                    >
+                      {tier.buttonText}
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </Container>
     </>
   );
