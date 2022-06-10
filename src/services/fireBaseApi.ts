45b1ced746a69df074d7f403e7696b587ded9b42
Matheus Ramos Kafuri <kafurimatheus@gmail.com>
2022-06-10 16:12:05 -0300

    firebase configuration

--- src/services/fireBaseApi.ts
+++ src/services/fireBaseApi.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const fireBaseApi = axios.create({
-  baseURL: "https://instagram-manager-reports.vercel.app/api/",
+  // baseURL: "https://instagram-manager-reports.vercel.app/api/",
+  baseURL: "https://main.di6hbd29wha4x.amplifyapp.com/api/",
   // baseURL: "http://localhost:3000/api/",
 });
 
